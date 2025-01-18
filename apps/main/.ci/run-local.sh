3b64c8328779c17a7dd003d608dbbc477a7dc794
wrporter <wesleyrporter@gmail.com>
2025-01-18 07:22:50 -0700

    Fix docker app

--- apps/main/.ci/run-local.sh
+++ apps/main/.ci/run-local.sh
@@ -9,6 +9,7 @@ docker run -it --init \
 	--rm \
 	--name=${APP_SCOPE} \
 	--env-file .env \
+	-e DATABASE_URL="postgresql://postgres:postgres@docker.host.internal:5432/commit" \
 	-p 3000:3000 \
 	-p 22500:22500 \
 	${IMAGE_PATH}
